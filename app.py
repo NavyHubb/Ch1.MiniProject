48bc7a5ee1d7e1af81408b6e8d9d4fcac767bc2a
gomcode <xorb.taegyu@gmail.com>
2022-08-04 12:09:53 +0900

    에어로빅 DB GET 함수 추가

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print('hello world')
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('13.125.254.186', 27017, username="test", password="test")
+db = client.movie_list
+
+@app.route('/')
+def work_list():
+    return render_template('work_list.html')
+
+@app.route('/work_list', methods=['GET'])
+def get_aerobic():
+    aerobic_list = list(db.aerobic.find({},{'_id':False}))
+    return jsonify({'aerobic_list': aerobic_list})
+
+if __name__ == '__main__': app.run('0.0.0.0', port=5000, debug=True)
