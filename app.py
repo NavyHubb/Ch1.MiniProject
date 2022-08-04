a5ecc0d2442697d2bdec43df537d33149ca2a6de
gomcode <xorb.taegyu@gmail.com>
2022-08-04 18:20:38 +0900

    port=5000으로 변경

--- app.py
+++ app.py
@@ -201,4 +201,4 @@ def get_movies():
         return redirect(url_for("home"))
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=80, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
